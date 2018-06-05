233a44b1a8499e391680ca0f4ca4fd2e242796a3
Artem Konstantinov <don666vito@yandex.ua>
2018-06-05 18:58:49 +0300

    Completed Section: Ajax Requests with React

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -17,18 +18,24 @@ constructor(props) {
      videos: [], 
      selectedVideo: null
     };
-
-  YTSearch({ key: API_KEY, term: 'minecraft' }, (videos) => {
-    this.setState({ 
-      videos: videos,
-      selectedVideo: videos[0]
-    });
-  });
+    this.videoSearch('surfboards');
 }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
+
     return (
       <div>
-          <SearchBar />
+          <SearchBar onSearchTermChange = { term => this.videoSearch(term) }/>
           <VideoDetails video={this.state.selectedVideo} />
           <VideoList
             onVideoSelect = { selectedVideo => this.setState({ selectedVideo }) }
